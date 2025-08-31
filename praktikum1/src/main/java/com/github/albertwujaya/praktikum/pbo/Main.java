efece94e7005e2445232a893f618b5d97cec80ba
albertwujaya <albertwujaya101@gmail.com>
2025-08-31 11:01:15 +0700

    Praktikum 1

---
+++ praktikum1/src/main/java/com/github/albertwujaya/praktikum/pbo/Main.java
@@ -0,0 +1,24 @@
+package com.github.albertwujaya.praktikum.pbo;
+
+public class Main {
+    public static void main(String[] args) {
+        String nama = "Albert Wujaya";
+        long NIM = 2407114002L;
+        int umur = 19;
+        String alamat = "Jalan Elang Sakti N0.2";
+        final String golonganDarah = "O";
+        double ip1 = 3.67;
+        double ip2 = 3.77;
+        float ip2Float = (float) 3.77;
+
+        System.out.println("         ---Biodata---   ");
+        System.out.println("Nama            : " + nama);
+        System.out.println("NIM             : " + NIM);
+        System.out.println("Umur            : "+ umur);
+        System.out.println("Alamat          : " + alamat);
+        System.out.println("Golongan Darah  : " + golonganDarah);
+        System.out.println("IP 1            : " + ip1);
+        System.out.println("IP 2            : " + ip2);
+        System.out.println("IP 2 Float      : " + ip2Float);
+    }
+}
