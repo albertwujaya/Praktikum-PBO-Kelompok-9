46acdf17d346592e9f0c70cca6f98a66fc8e5a87
albertwujaya <albertwujaya101@gmail.com>
2025-08-31 14:48:38 +0700

    insert ip ke array

--- praktikum1/src/main/java/com/github/albertwujaya/praktikum/pbo/Main.java
+++ praktikum1/src/main/java/com/github/albertwujaya/praktikum/pbo/Main.java
@@ -7,9 +7,8 @@ public class Main {
         int umur = 19;
         String alamat = "Jalan Elang Sakti N0.2";
         final String golonganDarah = "O";
-        double ip1 = 3.67;
-        double ip2 = 3.77;
-        float ip2Float = (float) 3.77;
+        double[] ip = {3.67, 3.77};
+        float ip2Float = (float) ip[1];
 
         System.out.println("         ---Biodata---   ");
         System.out.println("Nama            : " + nama);
@@ -17,8 +16,8 @@ public class Main {
         System.out.println("Umur            : "+ umur);
         System.out.println("Alamat          : " + alamat);
         System.out.println("Golongan Darah  : " + golonganDarah);
-        System.out.println("IP 1            : " + ip1);
-        System.out.println("IP 2            : " + ip2);
+        System.out.println("IP 1            : " + ip[0]);
+        System.out.println("IP 2            : " + ip[1]);
         System.out.println("IP 2 Float      : " + ip2Float);
     }
 }
